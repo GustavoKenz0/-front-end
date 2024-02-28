6fa3554e2d78cd9a2804a6f5432b74e5d7a4fc6a
GustoKenz0 <gustavokenzoiwakura189@gmail.com>
2024-02-28 11:49:59 -0300

    finalizando

--- aula 07/js/calculadora-imc.js
+++ aula 07/js/calculadora-imc.js
@@ -15,12 +15,31 @@ formulario.addEventListener('submit', function(e){
 
     const imc = calcularImc(peso, altura)
     console.log (imc)
-
-    alert(imc)
-
+    const classificacao = tabelaImc(imc)
+    mostrarResultado  (classificacao)
 })
 
 function calcularImc(peso, altura){
     let imc = (peso / altura ** 2 );
     return imc.toFixed(2);
 }
+
+function mostrarResultado(msg){
+    const resultado = document.querySelector("#resultado")
+    resultado.innerHTML = msg
+}
+
+
+function tabelaImc(imc){
+    const classificacao = ['abaixo do peso', 'peso normal', 'sobrepeso', 'obesidade grau I', 'obesidade grau II', 'obesidade grau III']
+
+    if (imc >= 39.9) return classificacao[5];
+    if (imc >= 34.9) return classificacao[4];
+    if (imc >= 29.9) return classificacao[3];
+    if (imc >= 24.9) return classificacao[2];
+    if (imc >= 18.5) return classificacao[1];
+    if (imc >= 18.5) return classificacao[1];
+    if (imc < 18.5) return classificacao[0];
+
+}
+
